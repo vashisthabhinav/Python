026b22b2a9d48069b0990a3d4977142e16b1fbac
Abhinav Vashisth <89785800+vashisthabhinav@users.noreply.github.com>
2022-03-23 07:12:04 +0530

    Add files via upload

---
+++ Intermediate/Object Oriented Programming/03_Practice.py
@@ -0,0 +1,13 @@
+from unicodedata import name
+
+
+class Programmer:
+    company = "Microsoft"
+    def __init__(self,name,product) :
+        self.name = name
+        self.product = product
+    def getInfo(self):
+        print(f"The name of the programmer is {self.name} and the product is {self.product}")
+Abhinav = Programmer("Abhinav", "Skype")
+Chirag = Programmer("Chirag", "MS")
+Abhinav.getInfo()
