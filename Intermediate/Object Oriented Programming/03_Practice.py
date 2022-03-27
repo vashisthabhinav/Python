a33aa22d5efbef7fccf58e5dcb064b730dc789f7
Abhinav Vashisth <89785800+vashisthabhinav@users.noreply.github.com>
2022-03-27 09:09:17 +0530

    Update 03_Practice.py

--- Intermediate/Object Oriented Programming/03_Practice.py
+++ Intermediate/Object Oriented Programming/03_Practice.py
@@ -1,6 +1,3 @@
-from unicodedata import name
-
-
 class Programmer:
     company = "Microsoft"
     def __init__(self,name,product) :
@@ -8,6 +5,22 @@ class Programmer:
         self.product = product
     def getInfo(self):
         print(f"The name of the programmer is {self.name} and the product is {self.product}")
+
 Abhinav = Programmer("Abhinav", "Skype")
 Chirag = Programmer("Chirag", "MS")
 Abhinav.getInfo()
+
+class Calculator:
+    def __init__(self,number) -> None:
+        self.number = number
+    def square(self):
+        print(f"The square of {self.number} is {self.number**2}")
+    def squareRoot(self):
+        print(f"The squareroot of {self.number} is {self.number**0.5}")
+    def cube(self):
+        print(f"The cube of {self.number} is {self.number**3}")
+
+a = Calculator(8100)
+a.square()
+a.squareRoot()
+a.cube() 
