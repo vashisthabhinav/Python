c47953126f4ab27fd99a4fb9bef8c8755ede657c
Abhinav Vashisth <89785800+vashisthabhinav@users.noreply.github.com>
2022-03-30 21:56:32 +0530

    Update 06_ClassMethods.py

--- Intermediate/Object Oriented Programming/06_ClassMethods.py
+++ Intermediate/Object Oriented Programming/06_ClassMethods.py
@@ -1,5 +1,6 @@
 # Class methods
+
 '''
 A class method is a method which is bound to the class and not the object of the class.
-@classmethod decorator is used to create a class method.
+  @classmethod decorator is used to create a class method.
 '''
